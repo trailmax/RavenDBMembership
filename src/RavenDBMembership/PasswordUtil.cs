edf249b8f5f87cf87761902234c2de3277c607d7
trailmax <trailmax1@gmail.com>
2013-03-17 00:07:09 +0000

    using SCrypt to save hashes. Added ScryptSharp package

--- src/RavenDBMembership/PasswordUtil.cs
+++ src/RavenDBMembership/PasswordUtil.cs
@@ -1,53 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Security.Cryptography;
+using CryptSharp;
 
 namespace RavenDBMembership
 {
-	public static class PasswordUtil
-	{
-		public static string CreateRandomSalt()
-		{
-			var saltBytes = new Byte[4];
-			var rng = new RNGCryptoServiceProvider();
-			rng.GetBytes(saltBytes);
-			return Convert.ToBase64String(saltBytes);
-		}
-
-		public static string HashPassword(string pass, string salt, string hashAlgorithm, string macKey)
-		{
-			byte[] bytes = Encoding.Unicode.GetBytes(pass);
-			byte[] src = Encoding.Unicode.GetBytes(salt);
-			byte[] dst = new byte[src.Length + bytes.Length];
-			Buffer.BlockCopy(src, 0, dst, 0, src.Length);
-			Buffer.BlockCopy(bytes, 0, dst, src.Length, bytes.Length);
-		    HashAlgorithm algorithm;
-			if (hashAlgorithm.ToUpper().Contains("HMAC"))
-            {
-                if (string.IsNullOrEmpty(macKey))
-                {
-                    throw new ArgumentException("HMAC style hashing algorithm requires a fixed ValidationKey in the web.config or machine.config.");
-                }
-                KeyedHashAlgorithm keyedAlg = KeyedHashAlgorithm.Create(hashAlgorithm);
-                keyedAlg.Key = HexToByte(macKey);
-                algorithm = keyedAlg;
-            }
-            else
-            {
-                algorithm = HashAlgorithm.Create(hashAlgorithm);
-            }
-			byte[] inArray = algorithm.ComputeHash(dst);
-			return Convert.ToBase64String(inArray);
-		}
-
-        private static byte[] HexToByte(string hexString)
+    public static class PasswordUtil
+    {
+        public static string CreateRandomSalt()
         {
-            byte[] returnBytes = new byte[hexString.Length / 2];
-            for (int i = 0; i < returnBytes.Length; i++)
-                returnBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
-            return returnBytes;
+            return Crypter.Blowfish.GenerateSalt(128);
         }
-	}
+
+        public static string HashPassword(string pass, string salt, string hashAlgorithm, string macKey)
+        {
+            // these number must not be changed, otherwise strings generated will be different and user's won't be able to login.
+            var derivedBytes = new byte[128];   // 128 is a number of bytes taken back from the encoding stream
+            const int cost = 1024; // cost of the algorithm - how many time we go round. Must be power of 2
+            const int blockSize = 8;    
+            const int parallel = 16;    // maximum number of threads to use. 
+            CryptSharp.Utility.SCrypt.ComputeKey(Encoding.ASCII.GetBytes(pass), Encoding.ASCII.GetBytes(salt), cost, blockSize, parallel, null, derivedBytes);
+
+            return derivedBytes.ToString();
+
+        }
+    }
 }
